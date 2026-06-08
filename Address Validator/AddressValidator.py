913bac769f5b92de1d1767c4e3341af9363eabfa
kiwi9009 <79302233+kiwi9009@users.noreply.github.com>
2026-06-08 16:51:43 +0700

    Implement email validation function
    Added email validation function with customizable parameters for input validation.

--- Address Validator/AddressValidator.py
+++ Address Validator/AddressValidator.py
@@ -1,3 +1,8 @@
+
+from typing import Any
+import re
+
+'''
 def addressVal(address):
     dot = address.find(".")
     at = address.find("@")
@@ -14,3 +19,41 @@ while(True):
     x = input("Input your email address:")
 
     addressVal(x)
+'''
+def email_validate(
+        prompt:str,
+        error_message:str,
+        pattern:str,   
+        strip:bool = False,
+        lower:bool = False,
+        )->str:
+            '''
+            This function will Using re validate to strong validate the input -> return str email
+            and its can reuse for phone, name ,address by change the pattern.
+            if try with validate number cant also expand more agriument: min_value,max_value or convert...
+            '''
+                while True:
+                    try:
+                        email = input(prompt).strip() if strip else input(prompt)
+                        email = email.lower() if lower else email
+                        if not re.match(pattern,email):
+                            raise ValueError
+                        else:
+                            return email         
+                               
+                    except ValueError as e:
+                        print(f'{error_message} {e}')
+               
+def main():     
+    email = email_validate(
+                prompt='Please enter your Email Address: ',
+                error_message='Invalid Email. Try again!!',
+                pattern = r'^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$',
+                strip=True,
+                lower=True
+                )
+
+    print(f'Your Email: {email}')
+    
+if __name__ == "__main__":
+    main()
